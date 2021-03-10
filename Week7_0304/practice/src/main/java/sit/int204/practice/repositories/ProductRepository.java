b2410d41ef8862587abeaab783d8ba18e177c024
Possawat Tanam <possawat.tanam@mail.ac.th>
2021-03-10 23:46:12 +0700

    [add] week7

---
+++ Week7_0304/practice/src/main/java/sit/int204/practice/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package sit.int204.practice.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import sit.int204.practice.models.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
